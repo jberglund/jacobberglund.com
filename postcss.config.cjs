0e8e3a0b08c7a7909bfbc2a1fb830165e2f98e8c
Jacob Berglund <jacob@jberglund.se>
2024-09-04 10:01:35 +0200

    typescales!

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,19 +1,19 @@
-const postcssPresetEnv = require("postcss-preset-env");
-const postcssGlobalData = require("@csstools/postcss-global-data");
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssGlobalData = require('@csstools/postcss-global-data');
 
 module.exports = {
   plugins: [
     postcssGlobalData({
-      files: ["./src/styles/media-queries.css"],
+      files: ['./src/styles/misc/media-queries.css'],
     }),
     postcssPresetEnv({
       stage: 3,
 
       features: {
-        "nesting-rules": true,
-        "custom-properties": false,
-        "custom-media-queries": true,
-        "media-query-ranges": true,
+        'nesting-rules': true,
+        'custom-properties': false,
+        'custom-media-queries': true,
+        'media-query-ranges': true,
       },
     }),
   ],
